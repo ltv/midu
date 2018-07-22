e44c80636b0f3df3f6e77da45b12d54b0ecc2fc3
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-07-22 11:21:08 +0700

    Fix jest rootDir

--- src/services/authorizers/__tests__/password.test.js
+++ src/services/authorizers/__tests__/password.test.js
@@ -1,18 +1,17 @@
 const { genHash, compareHash } = require('../helpers');
 
 describe('Password', () => {
-  const password = 'Abcd1234';
+  const password = 'Abcd1234XXX';
 
-  it('Should generate password hash', async () => {
+  it('Check generate hash', async () => {
     const hash = await genHash(password);
     expect(typeof hash).toBe('string');
     expect(hash.length).toBe(60);
   });
 
-  it('Should compare hash', async () => {
+  it('Check compare hash', async () => {
     const hash = await genHash(password);
     const matched = await compareHash(password, hash);
-    console.log('[matched]', matched);
     expect(matched).toBeTruthy();
   });
 });
