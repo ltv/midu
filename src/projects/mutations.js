dc87e6107a01795feac2c8ef67508c6d5c868b92
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-07-23 23:21:07 +0700

    Call to mongoose

--- src/projects/mutations.js
+++ src/projects/mutations.js
@@ -2,11 +2,13 @@ const { CREATE_PROJECT, UPDATE_PROJECT } = require('./constants');
 const { Project } = require('./models');
 
 const mutations = {
-  [CREATE_PROJECT]: (ctx, service) => {
-    return '[CREATE]';
+  [CREATE_PROJECT]: ctx => {
+    const { params } = ctx;
+    return Project.update({ name: params.name }, params, { upsert: true });
   },
-  [UPDATE_PROJECT]: (ctx, service) => {
-    return '[UPDATE]';
+  [UPDATE_PROJECT]: ctx => {
+    const { params } = ctx;
+    return Project.update({ name: params.name }, params, { upsert: true });
   }
 };
 
