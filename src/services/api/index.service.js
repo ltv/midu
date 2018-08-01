9c34081ebce3c5186b0c387417d0e5dddde82383
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-08-01 20:50:10 +0700

    Enable cors

--- src/services/api/index.service.js
+++ src/services/api/index.service.js
@@ -10,6 +10,22 @@ module.exports = {
   settings: {
     port: process.env.PORT || 3000,
 
+    // Global CORS settings for all routes
+    cors: {
+      // Configures the Access-Control-Allow-Origin CORS header.
+      origin: '*',
+      // Configures the Access-Control-Allow-Methods CORS header.
+      methods: ['GET', 'OPTIONS', 'POST', 'PUT', 'DELETE'],
+      // Configures the Access-Control-Allow-Headers CORS header.
+      allowedHeaders: [],
+      // Configures the Access-Control-Expose-Headers CORS header.
+      exposedHeaders: [],
+      // Configures the Access-Control-Allow-Credentials CORS header.
+      credentials: false,
+      // Configures the Access-Control-Max-Age CORS header.
+      maxAge: 3600
+    },
+
     routes: [
       {
         path: '/api',
